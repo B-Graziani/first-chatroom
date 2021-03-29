311d62ac0338aaee49c981d3443103f224778b12
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-03-29 09:46:24 +0200

    setup chatrroom et seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Chatroom.create!(name: "general")
+User.create!(nickname: "alice", email: "alice@example.org", password: "alice@example.org")
+User.create!(nickname: "bob", email: "bob@example.org", password: "bob@example.org")
